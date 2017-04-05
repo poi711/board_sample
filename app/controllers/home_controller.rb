bc8e72b78f8cd37e445a6152f066fe577d1d69b8
poi711 <poi711@likelion.org>
2017-04-05 10:21:59 +0000

    finish except UD

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
   end
+  def write
+
+    new_post = Post.new
+    new_post.name = params[:title]
+    new_post.content = params[:content]
+    new_post.save
+    
+    redirect_to '/list'
+  end
+  def list
+    @all_post = Post.all.order("id desc")
+  end
+  def rewrite
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get '/' => 'home/index'
+  get '/' => 'home#index'
+  post '/write' => 'home#write'
+  get '/list' => 'home#list'
   
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
